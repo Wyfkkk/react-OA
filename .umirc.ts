556b5fe92d56a289b20a839d5dcf4fadcb87c8b4
Wyfkkk <2224081986@qq.com>
2024-10-08 01:05:04 +0800

    初始化

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Wyfkkk 2224081986@qq.com
+ * @Date: 2024-10-08 00:48:28
+ * @LastEditors: Wyfkkk 2224081986@qq.com
+ * @LastEditTime: 2024-10-08 00:52:59
+ * @FilePath: \react项目\my-app\.umirc.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  // routes: [
+  //   { path: "/", component: "index" },
+  //   { path: "/docs", component: "docs" },
+  // ],
+  npmClient: 'npm',
+});
---
+++ src/pages/test.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: Wyfkkk 2224081986@qq.com
+ * @Date: 2024-10-08 00:53:47
+ * @LastEditors: Wyfkkk 2224081986@qq.com
+ * @LastEditTime: 2024-10-08 01:00:11
+ * @FilePath: \react项目\my-app\src\pages\test.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { Button } from 'antd';
+const test = () => {
+    return (
+      <div>
+        <p>这是我的test</p>
+        <Button type="primary">你好</Button>
+      </div>
+    );
+  };
+  
+  export default test;
